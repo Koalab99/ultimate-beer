457709e7edff73e2c73b69f1dd2ffacd5546bf7d
KillCo18 <corentin.arn@gmail.com>
2019-03-26 09:30:52 +0100

    Eminem - Lose yourself

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,13 +2,13 @@
 #include <Map.h>
 #include <iostream>
 
-const float g = 0.15f;
+const float g = 0.20f;
 
 Player::Player(Map *map) {
 	m_x = 2;
 	m_y = 20;
 	m_w = 1;
-	m_h = 2;
+	m_h = 1.5;
 	m_speed = 0;
 	m_floor = false;
 	m_map = map;
@@ -77,58 +77,67 @@ void Player::setMap(Map *map) {
 void Player::jump() {
 	if(m_floor) {
 		m_floor = false;
-		m_speed = 3.f;
+		m_speed = 2.f;
 	}
 }
 
 void Player::update() {
-	if(!m_floor) {
+	Bloc *r = m_map->collide(m_x + m_w + 1, m_y - 1);
+	Bloc *l = m_map->collide(m_x - 1, m_y - 1);
+	Bloc *tr = m_map->collide(m_x + m_w + 1, m_y + m_h + 1);
+	Bloc *tl = m_map->collide(m_x - 1, m_y + m_h + 1);
+	// If the player is supposed to not be touching the ground
+	if(!m_floor) { 
+		// Apply gravity
 		m_speed -= g;
+		// Making sure speed is not too big
 		if(m_speed > 2) {
 			m_speed = 2;
 		}
 		else if(m_speed < -2) {
 			m_speed = -2;
 		}
-		m_y += m_speed*0.3;
+		// To slow down the jump
+		m_y += m_speed*0.4;
 		if(m_map == nullptr) {
 			std::cerr << "Null map" << std::endl;
 			return;
 		}
-		Bloc *b = m_map->collide(m_x, m_y-1);
-		Bloc *c = m_map->collide(m_x + m_w, m_y-1);
-		Bloc *tr = m_map->collide(m_x + m_w, m_y + m_h);
-		Bloc *tl = m_map->collide(m_x, m_y + m_h);
-		if(m_map->blocExists(b) && m_map->blocExists(c) && !m_map->blocExists(tr) && !m_map->blocExists(tl)) {
-			m_y = b->GetY() + b->GetHeight() + 1;
+		if((m_map->blocExists(r) || m_map->blocExists(l)) && !m_map->blocExists(m_map->collide(m_x, m_y + m_h)) && !m_map->blocExists(m_map->collide(m_x + m_h, m_y + m_h))) {
+			if(m_map->blocExists(r)) {
+				m_y = r->GetY() + r->GetHeight()+1;
+			}
+			else {
+				m_y = l->GetY() + l->GetHeight() + 1;
+			}
 			m_speed = 0;
 			m_floor = true;
+		}
+		if(m_map->blocExists(r) && m_map->blocExists(l) && m_map->blocExists(tr) && m_map->blocExists(tl)) {
+			m_x -= m_direction;
 		}
 	}
 	else {
 		if(m_map == nullptr) {
 			return ;
 		}
-		if(!m_map->blocExists(m_map->collide(m_x, m_y-1.01)) && 
-				!m_map->blocExists(m_map->collide(m_x+m_w, m_y- 1.01))) {
-			m_floor = !m_floor;
-		}
-		Bloc *r = m_map->collide(m_x + m_w, m_y);
-		Bloc *l = m_map->collide(m_x, m_y);
-		if(m_map->blocExists(r) && !m_map->blocExists(l)) {
-			m_x--;
-		}
-		else if(m_map->blocExists(l) && !m_map->blocExists(r)) {
-			m_x ++;
+		if(!m_map->blocExists(m_map->collide(m_x + m_w, m_y - 2)) && !m_map->blocExists(m_map->collide(m_x, m_y - 2))) {
+			m_floor = false;
 		}
 	}
 	m_x += m_direction;
-
-	if(m_x < 0) {
-		m_x = 0;
+ 	if(m_map->blocExists(m_map->collide(m_x + m_w + 1, m_y)) && m_map->blocExists(m_map->collide(m_x + m_w + 1, m_y + m_h)) && !m_map->blocExists(m_map->collide(m_x -1, (m_y + m_y + m_h)/2))) {
+		m_x--;
 	}
-	else if(m_x >= m_map->getW()) {
-		m_x = m_map->getW()-1;
+	else if(m_map->blocExists(m_map->collide(m_x - 1, m_y)) && m_map->blocExists(m_map->collide(m_x - 1, m_y + m_h)) && !m_map->blocExists(m_map->collide(m_x + m_w + 1, (m_y + m_y + m_h)/2))) {
+		m_x++;
+	}
+
+	if(m_x < 1) {
+		m_x = 1;
+	}
+	else if(m_x >= m_map->getW()-1) {
+		m_x = m_map->getW()-2;
 	}
 }
 
