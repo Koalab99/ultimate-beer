b5fc437fbf4aa3c5e6f107c193f0be7d991db54b
BambouBlue <claire.beguin@etu.univ-lyon1.fr>
2019-03-26 10:23:18 +0100

    Item

--- inc/Item.h
+++ inc/Item.h
@@ -1,14 +1,34 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <Rectangle.h>
+#include <iostream>
+
 class Item {
 private:
+	
+	float m_x;
+    float m_y;
+	bool m_eaten;
 	int m_id;
+	Rectangle m_rectangle;
+	
 public:
 	Item();
+	Item(float, float);
 	Item(int id);
 	~Item();
+	
+	float GetX() const;
+    float GetY() const;
+    
+    void SetX(float);
+    void SetY(float);
+	
 	int getId() const;
+	void SetEaten(bool);
+	bool isEaten() const;
 };
 
 #endif
+
