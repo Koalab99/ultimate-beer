03aacaecc5e96d71a21dd17163353a9d602ab003
KillCo18 <corentin.arn@gmail.com>
2019-04-27 23:43:39 +0200

    Damn, appuyez sur escape pendant que vous jouez pour ouvrir le menu de pause + vider les fichiers inutiles

--- dep/PlayLevel.d
+++
@@ -1,3 +0,0 @@
-obj/PlayLevel.o: src/PlayLevel.cpp inc/PlayLevel.h inc/Map.h inc/Bloc.h \
- inc/Position.h inc/Enemy.h inc/Rectangle.h inc/Player.h inc/GameState.h \
- inc/StateReturnValue.h inc/PauseState.h inc/Button.h
--- dep/Player.d
+++
@@ -1,2 +0,0 @@
-obj/Player.o: src/Player.cpp inc/Player.h inc/Map.h inc/Bloc.h inc/Position.h \
- inc/Enemy.h inc/Rectangle.h
