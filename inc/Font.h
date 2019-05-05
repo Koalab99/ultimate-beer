ba3dacb64e610a78f656aa21fc564a65966aa596
Chocoluna <cl.coutupetra@gmail.com>
2019-05-05 18:28:32 +0200

    new comments, for a change

--- inc/Font.h
+++ inc/Font.h
@@ -2,7 +2,9 @@
 #define FONT_H
 
 #include <SDL2/SDL.h>
-// I hate to load 50000000 times the same font, so there it was.
+
+/*Defines a basic font that we can reuse here and there instead of defining a new one each we need it
+  Developper's comment: "I hate to load 50000000 times the same font, so there it was."*/
 
 const SDL_Color WHITE = { 255, 255, 255, 255 };
 const SDL_Color BLACK = { 0, 0, 0, 255 };
