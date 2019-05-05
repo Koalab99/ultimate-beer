c7577eb7fe098a4320f558f7f5b225402bc27667
Chocoluna <cl.coutupetra@gmail.com>
2019-05-05 20:22:55 +0200

    comments again and again

--- inc/Position.h
+++ inc/Position.h
@@ -1,6 +1,10 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+/*Defines what a position on the map is determined by:
+it has a vertical point and a horizontal point
+you can soustract or add positions together
+you can check if it is null and what to do if so  */
 class Position {
 private:
 	float m_x;
